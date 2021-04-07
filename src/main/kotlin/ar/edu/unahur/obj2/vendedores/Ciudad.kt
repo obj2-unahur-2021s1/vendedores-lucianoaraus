110c0cf893b3ba018611b7220362a751a50deef4
lucho <lucianoaraus97@gmail.com>
2021-04-07 11:15:31 -0300

    Agregue nueva clase (pais) y tambien cambie la condicion de esFirme() de 30 a 60 (ESTO ES UNA PRUEBA)

--- src/main/kotlin/ar/edu/unahur/obj2/vendedores/Ciudad.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/Ciudad.kt
@@ -2,4 +2,6 @@ package ar.edu.unahur.obj2.vendedores
 
 class Ciudad(val provincia: Provincia)
 class Provincia(val poblacion: Int)
+class Pais(val nombre: String, val provincia: Provincia)
+
 
--- src/main/kotlin/ar/edu/unahur/obj2/vendedores/Vendedor.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/Vendedor.kt
@@ -23,7 +23,7 @@ abstract class Vendedor { //abstract: No tiene implementacion, solo declaracion.
     certificaciones.add(certificacion)
   }
 
-  fun esFirme() = this.puntajeCertificaciones() >= 30
+  fun esFirme() = this.puntajeCertificaciones() >= 60
 
   fun certificacionesDeProducto() = certificaciones.count { it.esDeProducto }
   fun otrasCertificaciones() = certificaciones.count { !it.esDeProducto }
